fb5f3611890ed881d91d7f8208f6142614442c25
Shivam <sharmashivam1918@gmail.com>
2025-04-04 03:32:24 +0530

    port changed to 5000

--- index.js
+++ index.js
@@ -79,9 +79,9 @@ app.post("/logoutUsers", (req, res) => {
   res.clearCookie("token").json({ message: "Logged out successfully" });
 });
 
-// app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
