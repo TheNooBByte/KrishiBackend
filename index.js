8d6c33e44c5a49437da68a4400cadcd9a5cc6dad
Shivam <sharmashivam1918@gmail.com>
2025-10-04 05:34:26 +0530

    cors origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5500",
-      "https://theNooBByte.github.io",
+      "https://thenoobbyte.github.io",
     ],
     credentials: true,
   })
